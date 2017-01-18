dd1b154036b589b583a0a3a35881e716d15288c2
CG0323 <wangchun0323@qq.com>
2017-01-18 15:34:49 +0800

    add README file

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,6 @@ from fabric.api import local, env, settings, abort, run, cd, lcd
 from fabric.contrib.console import confirm
 import time
 
-# env.use_ssh_config = True
 env.hosts = ['60.205.216.128:5711']
 env.user = 'root'
 env.key_filename = 'C:\Users\mac\Documents\id_rsa_mopyfish'
